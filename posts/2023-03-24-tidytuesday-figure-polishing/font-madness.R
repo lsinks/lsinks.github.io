29fb35d88a4bf637694e22541fc9bc869294aa88
Louise E. Sinks <lsinks@users.noreply.github.com>
2023-04-10 12:34:38 -0400

    render tidymodels tutorial blog post

---
+++ posts/2023-03-24-tidytuesday-figure-polishing/font-madness.R
@@ -0,0 +1,19 @@
+
+loadfonts()
+
+windowsFonts("Cambria Math" = windowsFont("Cambria Math"))
+
+joined %>%
+  ggplot(aes(line_comment_token, n, size = log(number_of_users), 
+             color = log(number_of_users), label = title)) +
+  # geom_point() +
+  scale_y_log10() +
+  geom_text_repel(show.legend = FALSE) +
+  
+  ggtitle('\u00a3') +
+  #theme(text = element_text(family = "font1")) +
+  theme(text = element_text( family = "Cambria Math")) +
+  xlab("Token") +
+  ylab("Number of languages using token") 
+#theme_classic()
+
